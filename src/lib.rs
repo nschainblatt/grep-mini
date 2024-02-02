e8bad7493bfe054c8be9a5013a112a4b01b38bf7
nschainblatt <nschainblatt@gmail.com>
2024-02-01 21:55:07 -0500

    Optimized run and search functions, added unit test, got rid of unnecessary tests, refactored to use string slices with lifetimes insead of cloning String types

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,9 @@
 use ::std::error;
 use std::fs;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn build_config() {
-        let args = vec![
-            "".to_string(),
-            "cat".to_string(),
-            "test-file.log".to_string(),
-        ];
-        let _ = Config::build(&args).unwrap();
-    }
-
-    #[test]
-    fn getting_content() {
-        let args = vec![
-            "".to_string(),
-            "cat".to_string(),
-            "test-file.log".to_string(),
-        ];
-        let config = Config::build(&args).unwrap();
-        let _ = run(&config).unwrap();
-    }
-
-    #[test]
-    #[should_panic(expected = "not enough arguments")]
-    fn not_enough_args() {
-        let args = vec!["cat".to_string(), "test-file.log".to_string()];
-        let _ = Config::build(&args).unwrap();
-    }
-
-    #[test]
-    #[should_panic(expected = "No such file")]
-    fn file_not_found() {
-        let args = vec![
-            "".to_string(),
-            "cat".to_string(),
-            "final-paper.txt".to_string(),
-        ];
-        let config = Config::build(&args).unwrap();
-        let _ = run(&config).unwrap();
-    }
-
-    #[test]
-    fn search_contents() {
-        let args = vec![
-            "".to_string(),
-            "john".to_string(),
-            "test-file.log".to_string(),
-        ];
-        let config = Config::build(&args).unwrap();
-        let contents = run(&config).unwrap();
-        let results = search(&config.query, &contents);
-        assert_eq!(
-            results[0].contents,
-            "2024-01-28 09:15:03 INFO User john logged in from IP 192.168.1.10".to_string()
-        );
-    }
-}
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
-}
-
-pub struct FoundLine {
-    pub contents: String,
-    pub line_number: usize,
 }
 
 impl Config {
@@ -83,19 +18,49 @@ impl Config {
     }
 }
 
-pub fn run(config: &Config) -> Result<String, Box<dyn error::Error>> {
-    let contents = fs::read_to_string(&config.file_path)?;
-
-    Ok(contents)
+struct LineMatch<'a> {
+    contents: &'a str,
+    number: usize,
 }
 
-pub fn search(query: &String, contents: &String) -> Vec<FoundLine> {
-    let lines = contents.split("\n");
-    let mut found_lines: Vec<FoundLine> = Vec::new();
-    for (idx, line) in lines.enumerate() {
+pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    let result: Vec<LineMatch> = search(&config.query, &contents);
+    if result.len() > 0 {
+        for line in result {
+            println!("Line: {} -> {}", line.number, line.contents);
+        }
+    } else {
+        println!("No matches found");
+    }
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<LineMatch<'a>> {
+    let mut results: Vec<LineMatch<'a>> = Vec::new();
+    for (number, line) in contents.lines().enumerate() {
         if line.contains(query) {
-            found_lines.push(FoundLine { contents: line.to_string(), line_number: idx + 1 });
+            results.push(LineMatch {
+                contents: line,
+                number: number + 1,
+            });
         }
     }
-    found_lines
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."][0], search(query, contents)[0].contents);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,8 @@ fn main() {
         println!("Problem parsing arguments: {error}");
         process::exit(1);
     });
-    if let Err(error) = minigrep::run(&config) {
+    if let Err(error) = minigrep::run(config) {
         println!("{error}");
         process::exit(1);
     }
-    match minigrep::run(&config) {
-        Ok(contents) => {
-            let found_lines = minigrep::search(&config.query, &contents);
-            if found_lines.len() == 0 {
-                println!("Search argument not found in {}", &config.file_path);
-                process::exit(0);
-            }
-            println!("Found {} line/s", found_lines.len()); 
-            for line in found_lines {
-                println!("line: {} -- {} ", &line.line_number, &line.contents);
-            }
-
-        },
-        Err(error) => {
-            println!("{error}");
-            process::exit(1);
-        }
-    };
 }
